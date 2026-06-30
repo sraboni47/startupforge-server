87607d35336dfb875df61445903fcda894f39b3c
sraboni47 <sarkersraboni47@gmail.com>
2026-06-30 21:28:27 +0600

    vercel link change

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://startupforge-client-gamma.vercel.app",
+    ],
     credentials: true,
   })
 );
