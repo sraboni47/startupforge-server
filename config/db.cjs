1d1a66bf2fb61324cf677eb55b0718ef66149785
sraboni47 <sarkersraboni47@gmail.com>
2026-06-30 18:34:51 +0600

    file name change

--- config/db.cjs
+++ config/db.cjs
@@ -1,12 +1,5 @@
 
 
-
-
-
-
-
-
-
 require("dotenv").config();
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
--- index.cjs
+++ index.cjs
@@ -1,8 +1,3 @@
-
-
-
-
-
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
